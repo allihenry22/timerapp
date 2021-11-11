3b08a662a783ca04434445f6914ac30567108e65
Layne <layneingramtaylor@gmail.com>
2021-11-10 19:22:29 -0600

    WIP

--- src/init.js
+++ src/init.js
@@ -1,8 +1,16 @@
-import greetingMessage from './test-import.js';
+// import greetingMessage from './test-import.js';
 
-function init() {
-  console.log('Start App');
-  greetingMessage('Team Timer App');
+// function init() {
+//   console.log('Start App');
+//   greetingMessage('Team Timer App');
+// }
+
+// init();
+
+const onSubmit = (event) => {
+  debugger;
+  event.preventDefault();
+  console.log(event)
 }
 
-init();
+document.querySelector('#js-form').addEventListener('submit', onSubmit)
